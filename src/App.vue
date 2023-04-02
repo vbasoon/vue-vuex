8c90509b408cc78af3462cef2a14fbf119733734
vbasoon <vbasoon@gmail.com>
2023-04-02 15:37:19 +0300

    start vuex

--- src/App.vue
+++ src/App.vue
@@ -1,20 +1,33 @@
 <template>
-  <img alt="Vue logo" src="./assets/logo.png">
-  <HelloWorld msg="Welcome to Your Vue.js App"/>
+  <div>
+    <ProductList/>
+  </div>
+
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+
+import ProductList from '@/components/ProductList.vue'
 
 export default {
+  data() {
+    return {
+
+    }
+  },
+  created(){
+
+  },
   name: 'App',
   components: {
-    HelloWorld
+    ProductList
   }
 }
 </script>
 
 <style lang="scss">
+
+
 #app {
   font-family: Avenir, Helvetica, Arial, sans-serif;
   -webkit-font-smoothing: antialiased;
@@ -22,5 +35,10 @@ export default {
   text-align: center;
   color: #2c3e50;
   margin-top: 60px;
+
+  li {
+    list-style-type: none;
+    margin-bottom: 20px;
+  }
 }
 </style>
---
+++ src/components/ProductList.vue
@@ -0,0 +1,35 @@
+<template>
+  <div>
+  <h1>Product List</h1>
+  <ul>
+    <li v-for="product in products">
+      {{ product.title }} - {{ product.price }}
+    </li>
+  </ul>
+
+  </div>
+</template>
+
+<script>
+
+import shop from '@/api/shop.js'
+
+export default {
+  data() {
+    return {
+      products: [],
+    }
+  },
+  created(){
+    shop.getProducts(products => {
+      this.products = products
+    })
+  },
+  name: 'ProductList',
+
+}
+</script>
+
+<style lang="scss">
+
+</style>
